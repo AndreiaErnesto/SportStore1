70f0d49684e746d515aec11abb2b2ae305ec3e5f
AndreiaErnesto <32199098+AndreiaErnesto@users.noreply.github.com>
2017-11-16 10:56:40 +0000

    View Models para informação da pafinação (PagingInfo)
    Criação de PagingInfo
    Criação e utilização do TagHelper
    Passar informação dos produtos para o View Model da lista de produtos

--- SportStore/Controllers/ProductsController.cs
+++ SportStore/Controllers/ProductsController.cs
@@ -4,26 +4,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportStore.Models;
+using SportStore.Models.ViewModels;
 
 namespace SportStore.Controllers
 {
-    public class ProductsController : Controller {
+    public class ProductsController : Controller
+    {
         //need to get the repository - service
         private IProductRepository repository;
 
         public int PageSize = 4;
 
-        public ProductsController(IProductRepository repository) {
+        public ProductsController(IProductRepository repository)
+        {
             this.repository = repository;
         }
 
-        public ViewResult List(int page = 1) {
-            return View(
-                repository.Products
-                    .OrderBy(p => p.Price)
-                    .Skip(PageSize * (page - 1))
-                    .Take(PageSize)
-                );
-        }
+        public ViewResult List(int page = 1) => View(
+                new ProductsListViewModel
+                {
+
+                    Products = repository.Products
+                        .OrderBy(p => p.Price)
+                        .Skip(PageSize * (page - 1))
+                        .Take(PageSize),
+
+
+                    PagingInfo = new PagingInfo
+                    {
+                        CurrentPage = page,
+                        ItemsPerPage = PageSize,
+                        TotalItems = repository.Products.Count()
+
+                    }
+                }
+            );
     }
 }
---
+++ SportStore/InfrasStructure/PaginationLinksTagHelper.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SportStore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.InfrasStructure {
+    [HtmlTargetElement("div", Attributes = "page-model")]
+
+    public class PaginationLinksTagHelper : TagHelper {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public static int MaxLinksBeforeAndAfterCurrentPage = 7;
+
+        PaginationLinksTagHelper(IUrlHelperFactory urlHelperFactory) {
+            this.urlHelperFactory = urlHelperFactory;
+        }
+        [ViewContext][HtmlAttributeNotBound]
+
+        public ViewContext ViewContext { get; set; }
+        
+        public PagingInfo PageModel { get; set; } //Importajte - faz ligação com o page-model acima
+
+        public string PageAction { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output){
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+
+            TagBuilder result = new TagBuilder("div");
+            int initial = PageModel.CurrentPage - MaxLinksBeforeAndAfterCurrentPage;
+            if (initial < 1) initial = 1;
+
+            int final = PageModel.CurrentPage + MaxLinksBeforeAndAfterCurrentPage;
+            if (final > PageModel.TotalPages) final = PageModel.TotalPages;
+
+            for (int p = initial; p <= PageModel.TotalPages; p++) {
+                TagBuilder pageLink = new TagBuilder("a"); //tag da hiperligação
+                pageLink.Attributes["href"] = urlHelper.Action(PageAction, new { page = p}); //temos de ir ao atributo de href criar um link para a ação page = p
+
+                pageLink.InnerHtml.Append(p.ToString());
+
+                result.InnerHtml.AppendHtml(pageLink);
+            }
+            output.Content.AppendHtml(result.InnerHtml); //apenas conteudo da div
+        }
+    }
+}
