a4c78a8a45075b3137d60572cf4021d8f4d6bb97
AndreiaErnesto <32199098+AndreiaErnesto@users.noreply.github.com>
2017-11-23 10:38:27 +0000

    Melhoração do design da página da lista de produtos
    Colocação do preço à direita à frente do nome do produto dentro de uma label.
    Criação de propriedades no PaginationLinksTagHelper para melhoração dos links das páginas.
    Utilização dessas propriedades na view da lista nomeadamente na div da paginação.

--- SportStore/InfraStructure/PaginationLinksTagHelper.cs
+++ SportStore/InfraStructure/PaginationLinksTagHelper.cs
@@ -17,16 +17,18 @@ namespace SportStore.InfraStructure {
 
         public static int MaxLinksBeforeAndAfterCurrentPage = 7;
 
-        PaginationLinksTagHelper(IUrlHelperFactory urlHelperFactory) {
+        public PaginationLinksTagHelper(IUrlHelperFactory urlHelperFactory) {
             this.urlHelperFactory = urlHelperFactory;
         }
         [ViewContext][HtmlAttributeNotBound]
-
         public ViewContext ViewContext { get; set; }
-        
-        public PagingInfo PageModel { get; set; } //Importajte - faz ligação com o page-model acima
-
+        public PagingInfo PageModel { get; set; } //Importante - faz ligação com o page-model acima
         public string PageAction { get; set; }
+        //Para a paginação aprecer correta
+        public bool CssClassesEnable { get; set; } = false;
+        public string CssClassePage { get; set; }  //Css classe que quero em cada um dos links da pagina
+        public string CssClassePageNormal { get; set; }  //links da pagina normal
+        public string CssClassePageSelected { get; set; }  //links da pagina selecionada
 
         public override void Process(TagHelperContext context, TagHelperOutput output){
             IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
@@ -41,8 +43,12 @@ namespace SportStore.InfraStructure {
             for (int p = initial; p <= PageModel.TotalPages; p++) {
                 TagBuilder pageLink = new TagBuilder("a"); //tag da hiperligação
                 pageLink.Attributes["href"] = urlHelper.Action(PageAction, new { page = p}); //temos de ir ao atributo de href criar um link para a ação page = p
-
                 pageLink.InnerHtml.Append(p.ToString());
+
+                if (CssClassesEnable){
+                    pageLink.AddCssClass(CssClassePage);
+                    pageLink.AddCssClass(p == PageModel.CurrentPage ? CssClassePageSelected : CssClassePageNormal);
+                }
 
                 result.InnerHtml.AppendHtml(pageLink);
             }
